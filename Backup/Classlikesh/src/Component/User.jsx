a20d6a1792e986da6ae6898b3467b32d30c9c79a
siva krishna <sivakrishna053936@gmail.com>
2024-08-09 15:11:20 +0530

    test

--- Backup/Classlikesh/src/Component/User.jsx
+++
@@ -1,9 +0,0 @@
-import react from 'react'
-class User extends react.Component{
-    render(){
-        return <div>
-            <h1>This is a User component</h1>
-        </div>
-    }
-}
-export default User
---
+++ Backup/StatWEB BY REACT/src/App.js
@@ -0,0 +1,8 @@
+import Nav from './Components/Nav'
+let App=()=>{
+    return <div>
+        <h1>This is a App component</h1>
+        <Nav/>
+    </div>
+}
+export default App
