9e31b5a0d19694a268b4a571f69a6b341d2df443
siva krishna <sivakrishna053936@gmail.com>
2024-09-17 12:33:18 +0530

    test

--- PROJECTS/Begginner/Project1Homepage/src/App.jsx
+++ PROJECTS/Begginner/Project1Homepage/src/App.jsx
@@ -1,23 +1,11 @@
-import react from 'react'
+
+import Navigation  from './Components/Navigation'
+import HeroSection from './Components/HeroSection'
 import './App.css'
-// import "../public/images"
 let App=()=>{
-  return <>
-       <div>
-          <nav>
-            <div className='logo'>
-              <img src="../public/images/Logo.png" alt="" />
-            </div>
-            <ul>
-              <li href="#">Menu</li>
-              <li href="#">Location</li>
-              <li href="#">About</li>
-              <li href="#">Contact</li>
-            </ul>
-            <button>Login</button>
-          </nav>
-       </div>
-     
-  </>
+ return <>
+       <Navigation/>
+       <HeroSection/>
+     </>
 }
 export default App
---
+++ PROJECTS/Begginner/Project1Homepage/src/Components/HeroSection.jsx
@@ -0,0 +1,31 @@
+let HeroSection=()=>{
+    return<>
+   <main className="hero container">
+           <div className="HeroContent">
+              <h1>YOUR FEET DESERVE  <br />
+              THE BEST</h1>
+              <p>YOUR FEET DESRVE BETTER AND WE ARE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET  <br />
+              DESRVE BETTER AND WE ARE HERE TO HELP YOU WITH OUR SHOES.</p>
+              <div className="Buttons">
+                <button>Shop Now</button> <button className="secondary">Cetegory</button>
+              </div>  
+              <p id="Availablity">Also Available on</p>
+              <div className="Brandicons">
+                <img src="/images/flipkart.jpg" alt="" id="flipkart"/>
+                <img src="/images/Amazon.jpg" alt="" id="Amazon" />
+                
+
+              </div>
+              
+           </div>
+           <div className="Heroimage">
+            <img src="/images/AnotherShoe.webp" alt="" />
+
+           </div>
+         
+   
+  </main>
+    </>
+}
+export default HeroSection
+
