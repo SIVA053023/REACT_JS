73e9458a2852ade883a35e786ce4005db73df31b
siva krishna <sivakrishna053936@gmail.com>
2024-09-24 18:49:18 +0530

    test

---
+++ PROJECTS/Begginner/Project2Formpage/src/App.jsx
@@ -0,0 +1,10 @@
+import Navigation from "./Components/Navigation"
+import './App.css'
+
+let App=()=>{
+  return <div>
+    <Navigation/>
+  </div>
+}
+
+export default App
---
+++ PROJECTS/Begginner/Project2Formpage/src/Components/Navigation.jsx
@@ -0,0 +1,15 @@
+
+let Navigation=()=>{
+    return <>
+            <nav className="navbar">
+                <a href="#"><img src="/Images/Logo.avif" alt="" className="logo" /></a>
+                <ul>
+                    <li href="#">Home</li>
+                    <li href="#">About</li>
+                    <li href="#">Services</li>
+                    <li href="#">Contact</li>
+                </ul>
+            </nav>
+          </>
+}
+export default Navigation
