153aef66ae20393ef448325fcd0e5859e9b373dd
siva krishna <sivakrishna053936@gmail.com>
2024-10-03 12:23:31 +0530

    test

--- PROJECTS/Begginner/DicegameP3/src/App.jsx
+++ PROJECTS/Begginner/DicegameP3/src/App.jsx
@@ -5,7 +5,7 @@ import Gameplay from './Components/Gameplay';
 
 function App() {
 
-  const [isGamestarted, setisGamestarted]=useState(false);        // here we rae using the usestate
+  const [isGamestarted, setisGamestarted]=useState(false);        // here we are using the usestate
    const toggleGameplay=()=>{
     setisGamestarted((prev)=> !prev )
    }
