512f354f1763678c03b3f5f2b35eafdacc97e0ae
siva krishna <sivakrishna053936@gmail.com>
2024-10-20 23:17:15 +0530

    test

---
+++ main/src/Message/Message.jsx
@@ -0,0 +1,18 @@
+import './Style.css'
+
+let Message=()=>{
+    return <>
+     <h1>This is a onE COMPONENT</h1>
+     <nav>
+        <a href="#">Logo</a>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Services</li>
+        </ul>
+     </nav>
+    
+    </>
+}
+export default Message
---
+++ main/src/index.js
@@ -0,0 +1,3 @@
+import reactdom from 'react-dom'
+import App from './App'
+reactdom.render(<App/>,document.getElementById('root'))
