7fc3983fd071a740a799290cb932f934890ecd1d
siva krishna <sivakrishna053936@gmail.com>
2024-11-04 22:22:18 +0530

    test

--- PROJECTS/Begginner/FoodyZone/src/Components/Header/HeaderComponent.jsx
+++ PROJECTS/Begginner/FoodyZone/src/Components/Header/HeaderComponent.jsx
@@ -12,16 +12,7 @@ const HeaderComponent = () => {
       <div className="search">
       <input type="text" name="" id="" placeholder='search foood'/>
       </div>
-      <div className="nav">
-        <nav className='navbar'>
-            <ul>
-                <li><NavLink to="/Home">Home</NavLink></li>
-                <li><NavLink to="/About">About</NavLink></li>
-                <li><NavLink to="/Contact">Contact</NavLink></li>
-                <li><NavLink to="/Services">Services</NavLink></li>
-            </ul>
-        </nav>
-      </div>
+      
     </div>
   </>
 
